648d7811b7140eacf2cc74806eedd0a4a5a308e1
boom9881 <170857925@qq.com>
2014-09-12 09:57:37 +0800

    commit boom

--- WEB-INF/src/com/shuntian/portlet/intranet/action/AttendancePortlet.java
+++ WEB-INF/src/com/shuntian/portlet/intranet/action/AttendancePortlet.java
@@ -1,7 +1,14 @@
 package com.shuntian.portlet.intranet.action;
 
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+
 import com.liferay.util.bridges.mvc.MVCPortlet;
 
 public class AttendancePortlet extends MVCPortlet {
-
+	public void editUser(ActionRequest actionRequest,
+			ActionResponse actionResponse) {
+		
+		
+	}
 }
